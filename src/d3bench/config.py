9394f7649df2cf747ff6f0b1cef6416e808c8144
Borja Esteban <boressan@outlook.com>
2025-02-04 11:17:39 +0100

    fix!: Data_Energy preprocess to fit actual energy_data.csv

--- src/d3bench/config.py
+++ src/d3bench/config.py
@@ -3,9 +3,13 @@ import pathlib
 
 
 # Default data path
-_data_path = os.getenv("DATA_PATH", "/data")
+_data_path = os.getenv("DATA_PATH", "data")
 DATA_PATH = pathlib.Path(_data_path)
 
 # Default results path
-_results_path = os.getenv("RESULTS_PATH", "/results")
+_results_path = os.getenv("RESULTS_PATH", "results")
 RESULTS_PATH = pathlib.Path(_results_path)
+
+# Data datetime filter boundaries
+DATA_START_DATE = os.getenv("DATA_START_DATE", "2019-04-01")
+DATA_END_DATE = os.getenv("DATA_END_DATE", "2022-04-01")
