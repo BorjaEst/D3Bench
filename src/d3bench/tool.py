c33cc04d1b38bc4e7e68e4fe65593becb54cea4c
Borja Esteban <boressan@outlook.com>
2025-02-05 16:46:27 +0100

    chore: set abstract class for Tool

--- src/d3bench/tool.py
+++ src/d3bench/tool.py
@@ -1,16 +1,23 @@
+from abc import ABC, abstractmethod
 from enum import Enum
+from typing import Optional
 
 import nannyml as nml  # pip install nannyml
 import numpy as np
 import pandas as pd  # pip install pandas
 from alibi_detect.cd import CVMDrift, KSDrift, SpotTheDiffDrift
-from evidently import ColumnMapping
 from evidently.metric_preset import DataDriftPreset
-from evidently.metrics import *
+
+# from evidently.metrics import *
 from evidently.report import Report
+from pydantic import Field
+
+# pylint: disable=too-few-public-methods
 
 
-class METHODS(Enum):
+class Methods(Enum):
+    """Available methods for drift detection."""
+
     KOLMOGOROV_SMIRNOV = 0  # K-S Test
     WASSERSTEIN = 1  # Wasserstein Distance Normed
     KLD = 2  # Kullback-Leibler divergence
@@ -26,117 +33,135 @@ class METHODS(Enum):
     SPOTDIFF = 12  # Spot-The-Difference Test
 
 
-class Tool:
-    # Class attributes
-    name = "Tool"
+class ToolOptions:
+    """Settings to instantiate a tool."""
 
-    def __init__(self, name):
-        self.name = name
-        self.showReport = False
+    show_report: bool = Field(
+        default=False,
+        description="Show report if set.",
+    )
 
-    def preprocess(self):
-        pass
 
-    def runDriftdetection(self, ref, cur, building_id):
-        pass
+class Tool(ABC):
+    """Abstract class for drift detection tools."""
 
-    def __runDriftdetectiontest(self, building_id, test):
-        pass
+    # Abstract attribute to define by child class
+    name: str = ""
+    methods: set = set()
+
+    def __init__(self, settings: Optional[ToolOptions] = None):
+        settings = settings or ToolOptions()
+        self.show_report = settings.show_report
+
+    @abstractmethod
+    def __call__(self, df_train, df_test, method):
+        """Run drift detection on the reference and current data."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def preprocess(self, df_reference, df_current):
+        """Preprocess the data before drift detection."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def run_test(self, building_id, test):
+        """Run a specific drift detection test."""
+        raise NotImplementedError
 
 
 class Evidently(Tool):
-    def __init__(self, name, showReport=False):
-        super().__init__(name)
-        self.showReport = showReport
-        # self.methods = {}
-        self.methods = {
-            METHODS.WASSERSTEIN,
-            METHODS.KLD,
-            METHODS.PSI,
-            METHODS.JSD,
-            METHODS.AD,
-            METHODS.CVM,
-            METHODS.HD,
-            METHODS.MWURT,
-            METHODS.ED,
-            METHODS.ES,
-            METHODS.TT,
-            METHODS.KOLMOGOROV_SMIRNOV,
-        }
+    """Evidently drift detection tool."""
 
-    def preprocess(self):
-        if "consumption" in self.ref:
-            self.ref.rename(columns={"consumption": "target"}, inplace=True)
-            self.cur.rename(columns={"consumption": "target"}, inplace=True)
-            self.ref = self.ref.drop(columns={"ids"}).reset_index(drop=True)
-            self.cur = self.cur.drop(columns={"ids"}).reset_index(drop=True)
-            self.ref["target"] = pd.to_numeric(self.ref["target"])
-            self.ref["temp_outside"] = pd.to_numeric(self.ref["temp_outside"])
-        if "prob_predicted" in self.ref:
-            self.ref = self.ref.drop(columns={"prob_predicted", "predicted"})
-            self.cur = self.cur.drop(columns={"prob_predicted", "predicted"})
-        self.column_names = list(self.ref.columns)
+    name = "Evidently"
+    methods = {
+        Methods.WASSERSTEIN,
+        Methods.KLD,
+        Methods.PSI,
+        Methods.JSD,
+        Methods.AD,
+        Methods.CVM,
+        Methods.HD,
+        Methods.MWURT,
+        Methods.ED,
+        Methods.ES,
+        Methods.TT,
+        Methods.KOLMOGOROV_SMIRNOV,
+    }
 
-        column_mapping = ColumnMapping()
-        column_mapping.datetime = "time"
+    def preprocess(self, args____todo):
+        """Preprocess the data before drift detection."""
 
-    # @profile
-    def runDriftdetection(self, ref, cur, building_id):
-        self.ref = ref
-        self.cur = cur
-        self.preprocess()
+        # Make a copy of the dataframes
+        df_reference, df_current = dataset.splitTrainTest(building_id)
+        df_reference, df_current = df_reference.copy(), df_current.copy()
+
+        # If the column names are different, rename them
+        if "consumption" in df_reference:
+            # Rename the consumption column to target
+            df_reference.rename(columns={"consumption": "target"}, inplace=True)
+            df_current.rename(columns={"consumption": "target"}, inplace=True)
+            # Drop the ids column
+            df_reference.drop(columns={"ids"}, inplace=True)
+            df_current.drop(columns={"ids"}, inplace=True)
+            # Reset the index
+            df_reference.reset_index(drop=True, inplace=True)
+            df_current.reset_index(drop=True, inplace=True)
+            # Convert the columns to numeric
+            df_reference["target"] = pd.to_numeric(df_reference["target"])
+            df_reference["temp_outside"] = pd.to_numeric(df_reference["temp_outside"])
+
+        # If the columns are probabilities, drop them
+        if "prob_predicted" in df_reference:
+            # Drop the predicted and probability columns
+            df_reference.drop(columns={"prob_predicted", "predicted"}, inplace=True)
+            df_current.drop(columns={"prob_predicted", "predicted"}, inplace=True)
+
+        # Return the column names and the dataframes
+        return list(df_reference.columns), df_reference, df_current
+
+    def __call__(self, df_reference, df_current, building_id):
+        column_names, df_reference, df_current = self.preprocess(
+            df_reference, df_current
+        )
         my_dict = {}
         for test in self.methods:
-            if test == METHODS.WASSERSTEIN:
-                my_dict["Wasserstein Distanz"] = self.__runDriftdetectiontest(
+            if test == Methods.WASSERSTEIN:
+                my_dict["Wasserstein Distanz"] = self.run_test(
                     building_id, "wasserstein"
                 )
-            elif test == METHODS.KLD:
-                my_dict["K-L Divergence"] = self.__runDriftdetectiontest(
-                    building_id, "kl_div"
-                )
-            elif test == METHODS.PSI:
-                my_dict["PSI"] = self.__runDriftdetectiontest(building_id, "psi")
-            elif test == METHODS.JSD:
-                my_dict["J-S Distance"] = self.__runDriftdetectiontest(
-                    building_id, "jensenshannon"
-                )
-            elif test == METHODS.AD:
-                my_dict["Anderson-Darling"] = self.__runDriftdetectiontest(
-                    building_id, "anderson"
-                )
-            elif test == METHODS.CVM:
-                my_dict["Cramer-von-Mises"] = self.__runDriftdetectiontest(
+            elif test == Methods.KLD:
+                my_dict["K-L Divergence"] = self.run_test(building_id, "kl_div")
+            elif test == Methods.PSI:
+                my_dict["PSI"] = self.run_test(building_id, "psi")
+            elif test == Methods.JSD:
+                my_dict["J-S Distance"] = self.run_test(building_id, "jensenshannon")
+            elif test == Methods.AD:
+                my_dict["Anderson-Darling"] = self.run_test(building_id, "anderson")
+            elif test == Methods.CVM:
+                my_dict["Cramer-von-Mises"] = self.run_test(
                     building_id, "cramer_von_mises"
                 )
-            elif test == METHODS.HD:
-                my_dict["Hellinger-Distance"] = self.__runDriftdetectiontest(
-                    building_id, "hellinger"
-                )
-            elif test == METHODS.MWURT:
-                my_dict["Mann-Whitney U-Rank Test"] = self.__runDriftdetectiontest(
+            elif test == Methods.HD:
+                my_dict["Hellinger-Distance"] = self.run_test(building_id, "hellinger")
+            elif test == Methods.MWURT:
+                my_dict["Mann-Whitney U-Rank Test"] = self.run_test(
                     building_id, "mannw"
                 )
-            elif test == METHODS.ED:
-                my_dict["Energy-Distance"] = self.__runDriftdetectiontest(
-                    building_id, "ed"
-                )
-            elif test == METHODS.ES:
+            elif test == Methods.ED:
+                my_dict["Energy-Distance"] = self.run_test(building_id, "ed")
+            elif test == Methods.ES:
                 try:
-                    my_dict["Epps-Singleton"] = self.__runDriftdetectiontest(
-                        building_id, "es"
-                    )
+                    my_dict["Epps-Singleton"] = self.run_test(building_id, "es")
                 except:
                     my_dict["Epps-Singleton"] = "no result"
-            elif test == METHODS.TT:
-                my_dict["T-Test"] = self.__runDriftdetectiontest(building_id, "t_test")
-            elif test == METHODS.KOLMOGOROV_SMIRNOV:
-                my_dict["K-S Test"] = self.__runDriftdetectiontest(building_id, "ks")
+            elif test == Methods.TT:
+                my_dict["T-Test"] = self.run_test(building_id, "t_test")
+            elif test == Methods.KOLMOGOROV_SMIRNOV:
+                my_dict["K-S Test"] = self.run_test(building_id, "ks")
 
         return my_dict
 
-    # @profile
-    def __runDriftdetectiontest(self, building_id, test):
+    def run_test(self, building_id, test):
         # create TargetDriftReport in dict
         report = Report(metrics=[DataDriftPreset(stattest=test)])
         report.run(reference_data=self.ref, current_data=self.cur)
@@ -159,15 +184,15 @@ class Evidently(Tool):
 
 
 class NannyML(Tool):
-    def __init__(self, name, showReport=False):
-        super().__init__(name)
-        self.showReport = showReport
-        self.methods = {
-            METHODS.KOLMOGOROV_SMIRNOV,
-            METHODS.WASSERSTEIN,
-            METHODS.JSD,
-            METHODS.HD,
-        }
+    """NannyML drift detection tool."""
+
+    name = "NannyML"
+    methods = {
+        Methods.KOLMOGOROV_SMIRNOV,
+        Methods.WASSERSTEIN,
+        Methods.JSD,
+        Methods.HD,
+    }
 
     def preprocess(self):
         if "temp_outside" in self.ref:
@@ -183,29 +208,23 @@ class NannyML(Tool):
         self.column_names = [col for col in self.ref.columns if col != "time"]
 
     # @profile
-    def runDriftdetection(self, ref, cur, building_id):
+    def __call__(self, ref, cur, building_id):
         self.ref = ref
         self.cur = cur
         self.preprocess()
 
         my_dict = {}
         for test in self.methods:
-            if test == METHODS.KOLMOGOROV_SMIRNOV:
-                my_dict["K-S Test"] = self.__runDriftdetectiontest(
-                    building_id, "kolmogorov_smirnov"
-                )
-            elif test == METHODS.WASSERSTEIN:
-                my_dict["Wasserstein Distance"] = self.__runDriftdetectiontest(
+            if test == Methods.KOLMOGOROV_SMIRNOV:
+                my_dict["K-S Test"] = self.run_test(building_id, "kolmogorov_smirnov")
+            elif test == Methods.WASSERSTEIN:
+                my_dict["Wasserstein Distance"] = self.run_test(
                     building_id, "wasserstein"
                 )
-            elif test == METHODS.JSD:
-                my_dict["J-S Distance"] = self.__runDriftdetectiontest(
-                    building_id, "jensen_shannon"
-                )
-            elif test == METHODS.HD:
-                my_dict["Hellinger-Distance"] = self.__runDriftdetectiontest(
-                    building_id, "hellinger"
-                )
+            elif test == Methods.JSD:
+                my_dict["J-S Distance"] = self.run_test(building_id, "jensen_shannon")
+            elif test == Methods.HD:
+                my_dict["Hellinger-Distance"] = self.run_test(building_id, "hellinger")
 
         return my_dict
 
@@ -213,7 +232,7 @@ class NannyML(Tool):
     # drift score is the mean of all chunks
     # is_drifted is the mean of True/False depending on the threshold, computed to %
     # @profile
-    def __runDriftdetectiontest(self, building_id, test):
+    def run_test(self, building_id, test):
         calc = nml.UnivariateDriftCalculator(
             column_names=self.column_names,
             timestamp_column_name="time",
@@ -256,9 +275,13 @@ class NannyML(Tool):
 
 
 class AlibiDetect(Tool):
-    def __init__(self, name):
-        super().__init__(name)
-        self.methods = {METHODS.KOLMOGOROV_SMIRNOV, METHODS.CVM, METHODS.SPOTDIFF}
+
+    name = "AlibiDetect"
+    methods = {
+        Methods.KOLMOGOROV_SMIRNOV,
+        Methods.CVM,
+        Methods.SPOTDIFF,
+    }
 
     def preprocess(self):
         if "prob_predicted" in self.ref:
@@ -271,25 +294,23 @@ class AlibiDetect(Tool):
         self.ref = self.ref.to_numpy()
         self.cur = self.cur.to_numpy()
 
-    def runDriftdetection(self, ref, cur, building_id):
+    def __call__(self, ref, cur, building_id):
         self.ref = ref
         self.cur = cur
         self.preprocess()
 
         my_dict = {}
         for test in self.methods:
-            if test == METHODS.KOLMOGOROV_SMIRNOV:
-                my_dict["K-S Test"] = self.__runDriftdetectiontest("kolmogorov_smirnov")
-            elif test == METHODS.CVM:
-                my_dict["Cramer-von-Mises"] = self.__runDriftdetectiontest(
-                    "cramer_von_mises"
-                )
-            elif test == METHODS.SPOTDIFF:
-                my_dict["Spot-the-diff"] = self.__runDriftdetectiontest("spotdiff")
+            if test == Methods.KOLMOGOROV_SMIRNOV:
+                my_dict["K-S Test"] = self.run_test("kolmogorov_smirnov")
+            elif test == Methods.CVM:
+                my_dict["Cramer-von-Mises"] = self.run_test("cramer_von_mises")
+            elif test == Methods.SPOTDIFF:
+                my_dict["Spot-the-diff"] = self.run_test("spotdiff")
 
         return my_dict
 
-    def __runDriftdetectiontest(self, test):
+    def run_test(self, test):
         report_dict = {}
         my_dict = {}
 
