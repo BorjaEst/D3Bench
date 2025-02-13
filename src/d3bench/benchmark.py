838e6a8d23e2f4eb776d17ef5cae12752ab914ca
Borja Esteban <boressan@outlook.com>
2025-02-13 16:08:22 +0100

    fix: benchmark api and methods to new dataset and tools

--- src/d3bench/benchmark.py
+++ src/d3bench/benchmark.py
@@ -4,15 +4,15 @@ import logging
 import time
 import timeit
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 
-import numpy as np
 from memory_profiler import memory_usage
 from pydantic import Field
 
+from d3bench import utils
 from d3bench.dataset import Data
 from d3bench.tools import Tool
-from d3bench.utils import Criteria, Method, Report, Result
+from d3bench.utils import Criteria, Method, Report
 
 # pylint: disable=too-few-public-methods
 
@@ -27,6 +27,10 @@ class BenchmarkOptions:
     criteria: set[Criteria] = Field(
         default=set(["RUNTIME", "CPUTIME", "MEMORY"]),
         description="List of criteria to test.",
+    )
+    repetitions: int = Field(
+        default=3,
+        description="Number of repetitions to run the benchmark.",
     )
     vm: bool = Field(
         default=False,
@@ -44,17 +48,35 @@ class Benchmark:
         settings: Optional[BenchmarkOptions] = None,
     ):
         settings = settings or BenchmarkOptions()
+        self.criteria = settings.criteria
         self.tool = tool
         self.x_reference = data[0]
         self.x_test = data[1]
-        self.criteria = settings.criteria
-        self.on_vm = settings.vm
+        self.run_on_vm = settings.vm
+        self.repetitions = settings.repetitions
 
     def __iter__(self):
         """Run the benchmark with the given parameters."""
         for method in self.tool.methods:
             logger.debug("Running benchmark for %s", method)
             yield self(method)
+
+    @property
+    def test_info(self) -> utils.TestInformation:
+        """Return the information of the test method used in the benchmark."""
+        return utils.TestInformation(
+            framework=self.tool.name,
+            run_on_vm=self.run_on_vm,
+            repetitions=self.repetitions,
+        )
+
+    @property
+    def data_info(self) -> utils.DataInformation:
+        """Return the information of the data used in the benchmark."""
+        return utils.DataInformation(
+            len_traindata=self.x_reference.shape[0],
+            len_testdata=self.x_test.shape[0],
+        )
 
     def __call__(self, method: Method):
         """Run the benchmark with the given parameters."""
@@ -71,9 +93,12 @@ class Benchmark:
 
         # Prepare report for the benchmark
         logger.debug("Prep. benchmark report")
-        framework = self.tool.name  # tool used in the benchmark
-        n_points = self.x_reference.shape[0]  # number of points in the dataset
-        report = Report(framework, method, n_points, self.on_vm)
+        report = utils.Report(
+            test_information=self.test_info,
+            method=method,
+            detector_info=job.detector.info,
+            data_info=self.data_info,
+        )
 
         # Run the benchmark for each criterion
         logger.debug("Running benchmark for %s", self.criteria)
@@ -95,7 +120,7 @@ class Job:
         self.x_reference = tool.preprocess(benchmark.x_reference.copy())
         self.x_test = tool.preprocess(benchmark.x_test.copy())
         self.benchmark = benchmark
-        self.detector = tool[method]()
+        self.detector = tool[method](n_features=self.x_reference.shape[1])
 
     def fit(self) -> None:
         """Run the benchmark with the given parameters."""
@@ -106,8 +131,8 @@ class Job:
         self.detector.test(self.x_test)
 
     @property
-    def result(self) -> Result:
-        """Return the result of the test."""
+    def results(self) -> dict[str, Any]:
+        """Return the results of the benchmark."""
         return self.detector.result()
 
 
@@ -124,10 +149,7 @@ def run_functional(job: Job, report: Report):
     pass  # TODO: save the report
 
     # Collect the results of the drift detection
-    result = job.result
-    report.drift_detected = result.drift_detected
-    report.p_value = result.p_value
-    report.statistic = result.statistic
+    report.results = utils.Results(**job.results)
 
 
 def run_runtime(job: Job, report: Report):
@@ -142,13 +164,11 @@ def run_runtime(job: Job, report: Report):
     timer = timeit.Timer(job.test, timer=time.time)
 
     # Time runtimes measurements
-    _runtimes = timer.repeat(repeat=report.repetitions, number=1)
-    runtimes = np.array(_runtimes, dtype=float)
+    n_repeat = report.test_information.repetitions
+    runtimes = timer.repeat(n_repeat, number=1)
 
     # Compute statistics
-    report.runtime_avg = runtimes.mean()
-    report.runtime_max = runtimes.max()
-    report.runtime_min = runtimes.min()
+    report.runtime = utils.Stats(runtimes)
 
 
 def run_cputime(job: Job, report: Report):
@@ -163,13 +183,11 @@ def run_cputime(job: Job, report: Report):
     timer = timeit.Timer(job.test, timer=time.process_time)
 
     # Time runtimes measurements
-    _runtimes = timer.repeat(repeat=report.repetitions, number=1)
-    runtimes = np.array(_runtimes, dtype=float)
+    n_repeat = report.test_information.repetitions
+    runtimes = timer.repeat(n_repeat, number=1)
 
     # Compute statistics
-    report.cputime_avg = runtimes.mean()
-    report.cputime_max = runtimes.max()
-    report.cputime_min = runtimes.min()
+    report.cputime = utils.Stats(runtimes)
 
 
 def run_memory(job: Job, report: Report):
@@ -180,15 +198,14 @@ def run_memory(job: Job, report: Report):
 
     # Run the drift detection for each building
     # TODO: Future implementation for time training phase
-    rmem = [memory_usage(job.test) for _ in range(report.repetitions)]
+    n_repeat = report.test_information.repetitions
+    rmem = [memory_usage(job.test) for _ in range(n_repeat)]
 
     # Memory in run as the maximum memory used during the run
-    run_memories = np.array([max(mem) for mem in rmem], dtype=float)
+    run_memories = [max(mem) for mem in rmem]
 
     # Compute statistics
-    report.ram_avg = run_memories.mean()
-    report.ram_max = run_memories.max()
-    report.ram_min = run_memories.min()
+    report.ram = utils.Stats(run_memories)
 
 
 criteria_fn = {
